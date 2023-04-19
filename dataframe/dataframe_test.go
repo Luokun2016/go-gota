53c5a23406d8399d524d150907daaf4864dd249e
luokun <luokun@flexem.com>
2023-04-19 09:55:42 +0800

    change go mod

--- dataframe/dataframe_test.go
+++ dataframe/dataframe_test.go
@@ -10,7 +10,7 @@ import (
 
 	"math"
 
-	"github.com/go-gota/gota/series"
+	"github.com/Luokun2016/go-gota/series"
 )
 
 // compareFloats compares floating point values up to the number of digits specified.
--- dataframe/examples_test.go
+++ dataframe/examples_test.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/go-gota/gota/dataframe"
-	"github.com/go-gota/gota/series"
+	"github.com/Luokun2016/go-gota/dataframe"
+	"github.com/Luokun2016/go-gota/series"
 )
 
 func ExampleNew() {
